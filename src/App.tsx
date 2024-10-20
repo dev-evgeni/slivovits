2d6425cd094693e739cdd1192a28c62625be0c0e
dev-evgeni <dev.evgeni@gmail.com>
2024-10-20 16:06:04 +0300

    Added navbar-sidebar layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { DarkThemeToggle } from "flowbite-react";
+import NavbarSidebarLayout from "./layouts/navbar-sidebar";
 
 function App() {
   return (
-    <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
-      <h1 className="text-2xl dark:text-white">Flowbite React + Vite</h1>
-      <DarkThemeToggle />
-    </main>
+    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+      <NavbarSidebarLayout />
+    </div>
   );
 }
 

1cd6eb37f4ee05b4adf01a244808281808bc801e
dev-evgeni <dev.evgeni@gmail.com>
2024-10-21 18:16:22 +0300

    Fixed app height

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import NavbarSidebarLayout from "./layouts/navbar-sidebar";
 
 function App() {
   return (
-    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+    <div class="h-full antialiased bg-gray-50 dark:bg-gray-900">
       <NavbarSidebarLayout />
     </div>
     
